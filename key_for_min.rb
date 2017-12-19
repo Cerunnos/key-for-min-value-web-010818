e0e81c46d80c1b09158307a909fcddd6f5fa0ebb
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-19 20:09:06 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_var=0
-  value_var=0
+  key_var=nil
+  value_var=nil
   name_hash.each do |key,value|
-    if value_var=0 || value<value_var
+    if value_var==nil || value<value_var
       key_var=key
       value_var=value
     end
